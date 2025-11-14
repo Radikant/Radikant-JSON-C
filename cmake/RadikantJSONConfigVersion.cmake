87122d87fa53f91b57c2722c14f7552f69777a07
CharlesMcBaker <him@charlie-elbakry.nl>
2025-11-15 00:15:10 +0100

    Delete cmake/RadikantJSONConfigVersion.cmake

--- cmake/RadikantJSONConfigVersion.cmake
+++
@@ -1,7 +0,0 @@
-# RadikantJSONConfigVersion.cmake
-set(PACKAGE_VERSION "0.1.0")
-
-# Optional: fail if an incompatible version is requested
-if(PACKAGE_VERSION VERSION_LESS "0.1.0")
-    message(FATAL_ERROR "Installed RadikantJSON version is too old")
-endif()
